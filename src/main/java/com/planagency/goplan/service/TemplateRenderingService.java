2f012f6d73c34fe82c65b35b01cea43e28450eca
Epic <epicboyyt9@gmail.com>
2026-05-04 00:01:22 +0300

    feat: Remove unused template files and refactor template rendering method
    clean ups

--- src/main/java/com/planagency/goplan/service/TemplateRenderingService.java
+++ src/main/java/com/planagency/goplan/service/TemplateRenderingService.java
@@ -18,24 +18,7 @@ public class TemplateRenderingService {
 
     private final Configuration freeMarkerConfig;
 
-    public String renderTemplate(String templateName, Map<String, Object> variables){
-
-    try{
-        
-        Template template = freeMarkerConfig.getTemplate(templateName);
-        StringWriter writer = new StringWriter();
-        template.process(variables, writer);
-        return writer.toString();
-
-    }catch(Exception e){
-
-        log.error("Error loading template: {}", templateName, e);
-        throw new RuntimeException("Template Engine failure " + templateName, e);
-    }
-
-}
-
-public String renderTemplateFromGithub(String templateName, String rawTemplateContent, Map<String, Object> variables){
+    public String renderTemplateFromGithub(String templateName, String rawTemplateContent, Map<String, Object> variables){
     try{
     
         Template template = new Template(templateName, new StringReader(rawTemplateContent), freeMarkerConfig);
